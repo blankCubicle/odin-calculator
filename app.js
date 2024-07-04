ff9c08e9f8c1d2976345d1d45da7a79066bc6b32
blankCubicle <138397220+blankCubicle@users.noreply.github.com>
2024-07-04 15:00:09 -0400

    Add initial layout

---
+++ app.js
@@ -0,0 +1,31 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  if (b === 0) return 'ERROR';
+  return a / b;
+};
+
+const operate = function (a, b, operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return 'ERROR';
+  }
+};
